d28bd677daa49effe95d907bbfd1fdc7f25f48cd
YouHoGeon <me@youhogeon.com>
2021-08-27 22:48:09 +0900

    20210826

--- src/main/java/com/youhogeon/acmicpc/Main.java
+++ src/main/java/com/youhogeon/acmicpc/Main.java
@@ -4,7 +4,7 @@ import com.youhogeon.unsolved.*;
 // class Q14502{
 // 	int N, M;
 
-// 	public Q14502() throws Exception{
+// 	public Q14502() throws IOException{
 // 		Scanner sc = new Scanner(System.in);
 
 		
@@ -19,44 +19,71 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) {
 		try{
-			Q2448 q = new Q2448();
+			Q10830 q = new Q10830();
 			
 			q.solve();
 			//System.out.println(q.solve());
-		}catch(Exception e){
+		}catch(IOException e){
 			System.out.println(e.toString());
 		}
 	}
 }
 
-class Q2448{
+class Q10830{
 	int N;
-	char[][] arr;
+	long B;
+	int[][] A;
 
-	public Q2448() throws Exception{
+	public Q10830() throws IOException{
 		Scanner sc = new Scanner(System.in);
 
 		N = sc.nextInt();
+		B = sc.nextLong();
+
+		A = new int[N][N];
+
+		for (int i = 0; i < N; i++){
+			for (int j = 0; j < N; j++) A[i][j] = sc.nextInt() % 1000;
+		}
 
 		sc.close();
 	}
 
 	public void solve(){
-		int K = (int)(Math.log(N / 3) / Math.log(2));
-		int size = (K + 1) * 5 + (int)Math.pow(2, K) - 1;
-		arr = new char[size][size];
-
-		makeTri(K);
-
-		System.out.println(size);
+		A = power(A, B);
+		
+		for (int i = 0; i < N; i++){
+			for (int j = 0; j < N; j++){
+				System.out.print(Integer.toString(A[i][j]));
+				if (j != N - 1) System.out.print(" ");
+			}
+			System.out.println();
+		}
 	}
 
-	public char[][] makeTri(int K){
-		int size = (K + 1) * 5 + (int)Math.pow(2, K) - 1;
-		char[][] arr = new char[size][size];
+	public int[][] power(int[][] A, long B){
+		if (B == 1) return A;
 
-		if (K == 0){
-			char[0][0] = ' ';
+		int[][] A2 = power(A, B / 2);
+		A2 = mul(A2, A2);
+		if (B % 2 == 1) A2 = mul(A2, A);
+
+		return A2;
+	}
+
+	public int[][] mul(int[][] A, int[][] B){
+		int[][] C = new int[N][N];
+
+		for (int i = 0; i < N; i++){
+			for (int j = 0; j < N; j++){
+				for (int k = 0; k < N; k++){
+					C[i][j] += A[i][k] * B[k][j];
+					C[i][j] %= 1000;
+				}
+				C[i][j] %= 1000;
+			}
 		}
+
+		return C;
 	}
 }
