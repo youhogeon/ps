873b5f1957578e182d187b12e47739a7e9e23056
YouHoGeon <me@youhogeon.com>
2021-09-04 21:41:32 +0900

    20210904

--- src/main/java/com/youhogeon/acmicpc/Main.java
+++ src/main/java/com/youhogeon/acmicpc/Main.java
@@ -1,5 +1,4 @@
 package com.youhogeon.acmicpc;
-import com.youhogeon.unsolved.*;
 
 // class Q14502{
 // 	int N, M;
@@ -16,87 +15,16 @@ import com.youhogeon.unsolved.*;
 
 import java.util.*;
 import java.io.*;
+//import com.youhogeon.unsolved.*;
 
 public class Main {
 	public static void main(String[] args) {
 		try{
-			Q1202 q = new Q1202();
+			Q2467 q = new Q2467();
 			
-			//q.solve();
-			//System.out.println(q.solve());
+			System.out.println(q.solve());
 		}catch(IOException e){
 			System.out.println(e.toString());
-		}
-	}
-}
-
-class Q1202{
-	int N, M;
-	List<Integer> bags = new ArrayList<Integer>();
-	PriorityQueue<Item> items = new PriorityQueue<Item>();
-
-	public Q1202() throws IOException{
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-		String[] s = bf.readLine().split(" ");
-		N = Integer.parseInt(s[0]);
-		M = Integer.parseInt(s[1]);
-
-		for (int i = 0; i < N; i++){
-			s = bf.readLine().split(" ");
-			items.offer(new Item(Integer.parseInt(s[0]), Integer.parseInt(s[1])));
-		}
-
-		for (int i = 0; i < M; i++) bags.add(Integer.parseInt(bf.readLine()));
-	}
-
-	public long solve(){
-		long sum = 0;
-
-		bags.sort(Comparator.naturalOrder());
-
-		while (0 < M && !items.isEmpty()){
-			Item item = items.poll();
-			int idx = findItem(item.w);
-
-			if (idx == -1) continue;
-
-			M--;
-			sum += item.v;
-			bags.remove(idx);
-		}
-
-		return sum;
-	}
-
-	public int findItem(int n){
-		int left = 0;
-		int right = M - 1;
-		int mid = 0;
-		
-		while (left <= right){
-			mid = (left + right) / 2;
-
-			if (bags.get(mid) < n) left = mid + 1;
-			else right = mid - 1;
-		}
-
-		return (bags.get(mid) < n)?-1:mid;
-	}
-
-	class Item implements Comparable<Item>{
-		int w, v;
-
-		public Item(int w, int v){
-			this.w = w;
-			this.v = v;
-		}
-
-		public int compareTo(Item i){
-			if (this.v > i.v) return -1;
-			if (this.v < i.v) return 1;
-
-			return 0;
 		}
 	}
 }
