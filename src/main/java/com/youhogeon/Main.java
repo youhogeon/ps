6686f83dc9a588cac74247f60a31d2eac2e908fb
YouHoGeon <me@youhogeon.com>
2022-07-16 22:01:01 +0900

    20220716

--- src/main/java/com/youhogeon/Main.java
+++ src/main/java/com/youhogeon/Main.java
@@ -20,7 +20,7 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) {
 		try{
-			Q2098 q = new Q2098();
+			Q20040 q = new Q20040();
 			
 			System.out.println(q.solve());
 		}catch(IOException e){
@@ -29,15 +29,50 @@ public class Main {
 	}
 }
 
-class Q2098{
+class Q20040{
 	int N, M;
+	int[] head;
+	int result = 0;
 
-	public Q2098() throws IOException{
+	public Q20040() throws IOException{
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
+		String[] str = bf.readLine().split(" ");
+		N = Integer.parseInt(str[0]);
+		M = Integer.parseInt(str[1]);
+
+		head = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			head[i] = i;
+		}
+
+		for (int i = 0; i < M; i++) {
+			str = bf.readLine().split(" ");
+
+			int a = Integer.parseInt(str[0]), b = Integer.parseInt(str[1]);
+
+			if (i % 2 == 0) {
+				union1(a, b);
+				if (find1(a) == find1(b) && result == 0) result = i + 1;
+			} else {
+				union2(a, b);
+				if (find2(a) == find2(b) && result == 0) result = i + 1;
+			}
+		}
+	}
+
+	public int find(int a) {
+		while (a != head[a]) a = head[a]; 
+		
+		return a;
+	}
+
+	public void union(int a, int b) {
+		head[find(a)] = find(b);
 	}
 
 	public int solve(){
-
+		return result;
 	}
 }
