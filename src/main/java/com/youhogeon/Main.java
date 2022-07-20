ab64806f17107ee19c30e824c6ea7049d3994d7e
YouHoGeon <me@youhogeon.com>
2022-07-20 19:27:09 +0900

    20220720

--- src/main/java/com/youhogeon/Main.java
+++ src/main/java/com/youhogeon/Main.java
@@ -4,16 +4,16 @@ package com.youhogeon;
 // 	int N, M;
 
 // 	public Q14502() throws IOException {
-//		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+// 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-//		String[] str = bf.readLine().split(" ");
-//		N = Integer.parseInt(str[0]);
-//		M = Integer.parseInt(str[1]);
+// 		String[] str = bf.readLine().split(" ");
+// 		N = Integer.parseInt(str[0]);
+// 		M = Integer.parseInt(str[1]);
 // 	}
 
-//	public int solve() {
-//		return 0;
-//	}
+// 	public int solve() {
+// 		return 0;
+// 	}
 // }
 
 import java.util.*;
@@ -22,7 +22,7 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) {
 		try{
-			Q14939 q = new Q14939();
+			Q1509 q = new Q1509();
 			
 			System.out.println(q.solve());
 		}catch(IOException e){
---
+++ src/main/java/com/youhogeon/acmicpc/Q1509.java
@@ -0,0 +1,52 @@
+package com.youhogeon.acmicpc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Q1509{
+	//Q2079와 완전히 동일
+	char[] str;
+	int N;
+	boolean[][] dp;
+
+	public Q1509() throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+		str = bf.readLine().toCharArray();
+		N = str.length;
+		dp = new boolean[N][N];
+
+		for (int i = 0; i < N; i++) dp[i][i] = true;
+	}
+
+	public int solve() {
+		checkPalindrom();
+
+		int[] result = new int[N];
+		result[0] = 1;
+
+		for (int i = 1; i < N; i++) {
+			if (dp[0][i]) result[i] = 1;
+			else {
+				result[i] = 0x7FFFFFFF;
+
+				for (int j = 1; j <= i; j++) {
+					if (dp[j][i]) result[i] = Math.min(result[i], result[j - 1] + 1);
+				}
+			}
+		}
+
+		return result[N - 1];
+	}
+
+	public void checkPalindrom() {
+		for (int i = N - 1; i >= 0; i--) {
+			for (int j = N - 1; j > i; j--) {
+				if (i == j || str[i] != str[j]) continue;
+
+				if (j - i == 1 || dp[i + 1][j - 1]) dp[i][j] = true;
+			}
+		}
+	}
+}
